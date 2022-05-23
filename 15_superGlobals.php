fb32954d7de75f25e4fb0b6eb286396e9fc98694
Hari <Khariprasath351@gmail.com>
2022-05-23 16:09:40 +0530

    learned about superglobals in php

---
+++ 15_superGlobals.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
+NAME: <input type="text" name='firstname'>
+<input type="submit">
+</form>
+
+
+
+<?php
+
+
+
+
+//SUPERGLOBALS
+
+/*
+$GLOBALS--
+
+$_SERVER
+$_REQUEST
+$_POST
+$_GET
+$_FILES
+$_ENV
+$_COOKIES
+$_SESSION
+*/
+
+
+//Globlas
+
+// $x=15;
+
+// // $GLOBALS=array("x" =>5,)
+
+// function name(){
+//     echo $GLOBALS['x'];
+// };
+
+// name();
+// echo $_SERVER['PHP_SELF'];
+// echo '<br>';
+// echo $_SERVER['SERVER_NAME'];
+// echo '<br>';
+// echo $_SERVER['HTTP_HOST'];
+// echo '<br>';
+// echo $_SERVER['HTTP_REFERER'];
+// echo '<br>';
+// echo $_SERVER['HTTP_USER_AGENT'];
+// echo '<br>';
+// echo $_SERVER['SCRIPT_NAME'];
+// echo '<br>';
+
+
+//REQUEST
+if($_SERVER["REQUEST_METHOD"] == 'get'){
+    $name = $_GET["firstname"];
+
+    if(empty($name)){
+        echo "Name is empty";
+    }else{
+        echo "YOUR NAME IS " . $name;
+    }
+}
+
+?>
+
+</body>
+</html>
+
+
