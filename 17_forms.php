90f865a0c9fee8bf052d5df366be132eb504aae6
Hari <Khariprasath351@gmail.com>
2022-05-24 18:56:28 +0530

    form get and post

---
+++ 17_forms.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forms PHP</title>
+</head>
+<body>
+    <form action="./demo.php" method="POST">
+    Name: <input type="text" name="name" required><br><br>
+    Email: <input type="email" name="email"reqired><br><br>
+    <input type="submit" >
+  
+    </form>
+</body>
+</html>
